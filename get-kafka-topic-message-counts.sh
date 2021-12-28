48d301583a5488d89329342478505a8a630df5da
Sabarish Sasidharan <sabarishs@users.noreply.github.com>
2021-12-28 21:29:16 +0530

    Create get-kafka-topic-message-counts.sh

---
+++ get-kafka-topic-message-counts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Topic:Partition:Offset:Count"
+for i in $(kubectl exec -it kafka-0 -c kafka -- bash -c "kafka-run-class kafka.tools.GetOffsetShell --broker-list kafka-0:9093 --time $1 --topic device-status-sqdf")
+do
+  topic=$(echo $i|cut -d ":" -f 1)
+  partition=$(echo $i|cut -d ":" -f 2)
+  offset=$(echo $i|cut -d ":" -f 3|tr -d '\r \n')
+  echo "$topic:$offset:$partition:$(kubectl exec -it kafka-0 -c kafka -- bash -c "kafka-console-consumer --offset $offset --bootstrap-server kafka-0:9093 --topic $topic --partition $partition --timeout-ms 2000 2>/dev/null | wc -l ")"
+done
